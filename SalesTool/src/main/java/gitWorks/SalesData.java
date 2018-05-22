12836b978889720eab6b7ca2cb549c25e2c9c592
Winston.vijay <Winston.vijay@GSR-Lap31.GSRCHENNAI.LOCAL>
2018-05-22 14:48:56 +0530

    code reviewed-display() method

--- SalesTool/src/main/java/gitWorks/SalesData.java
+++ SalesTool/src/main/java/gitWorks/SalesData.java
@@ -4,8 +4,8 @@ public class SalesData {
 
 	int data[] = {0, 4, 42, 55, 66}; /*-?|first_review|Winston.vijay|c0|?*/
 	
-	//display method
-	public void display() {
+	//display method /*-?|SalesData.java-display() method|Winston.vijay|c0|*/
+	public void display() { /*-|SalesData.java-display() method|Winston.vijay|c0|?*/
 		System.out.println("Data: ");
 		//using the for-each loop
 		for(int k:data) {
